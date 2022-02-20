c59662f1d4ca86b7791161a4603bac77cab5107c
abhinandan <abhinandanc3388@gmail.com>
2022-02-20 21:51:50 +0530

    commit11

--- vars/Sharedlib.groovy
+++ vars/Sharedlib.groovy
@@ -1,3 +1,27 @@
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.Month
+import java.time.temporal.TemporalAdjuster
+import java.time.temporal.TemporalAdjusters
+
 def call(names){
     echo "Hey ${names} , How are you ?"
+
+    def flag=false
+
+    def currDate = LocalDate.now()
+    def thirdDay = currDate.with(TemporalAdjusters.dayOfWeekInMonth(3, DayOfWeek.SUNDAY));
+
+    if (currDate.equals(thirdDay)){
+        flag = true
+    }
+
+    echo "${flag}"
+
 }
+
+
+
+
+
