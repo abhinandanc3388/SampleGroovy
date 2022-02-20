a823980d4cbd8db2473a333389b4260ee99c8f79
abhinandan <abhinandanc3388@gmail.com>
2022-02-20 22:22:31 +0530

    commit11

--- vars/Sharedlib.groovy
+++ vars/Sharedlib.groovy
@@ -1,31 +1,25 @@
 import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.Month
-import java.time.temporal.TemporalAdjuster
 import java.time.temporal.TemporalAdjusters
 
-def call(names){
+def call(names) {
     echo "Hey ${names} , How are you ?"
 
-    def flag=false
+    def flag = false
 
     def currDate = LocalDate.now()
     def thirdDay = currDate.with(TemporalAdjusters.dayOfWeekInMonth(3, DayOfWeek.SUNDAY));
 
-    if (currDate.equals(thirdDay)){
+    if (currDate.equals(thirdDay)) {
         flag = true
     }
 
     echo "${flag}"
 
 
-
-   def runningBuilds = Jenkins.instance.getItems().collect { job->
-        job.builds.findAll { it.getResult().equals(null) }
-    }.flatten()
-
-    echo "${runningBuilds}"
+    Jenkins.instance.getAllItems(AbstractItem.class).each {
+        println it.fullName + " - " + it.class
+    };
 }
 
 
