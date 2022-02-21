65d418f11f488ed6433257f93964399bc650f1e6
abhinandan <abhinandanc3388@gmail.com>
2022-02-21 16:24:03 +0530

    commit11

--- vars/Sharedlib.groovy
+++ vars/Sharedlib.groovy
@@ -1,20 +1,18 @@
-import java.time.DayOfWeek
-import java.time.LocalDate
-import java.time.temporal.TemporalAdjusters
-
 def call(names) {
     echo "Hey ${names} , How are you ?"
 
     def flag = false
 
-    def currDate = LocalDate.now()
-    def thirdDay = currDate.with(TemporalAdjusters.dayOfWeekInMonth(3, DayOfWeek.SUNDAY));
-
+    //  def currDate = LocalDate.now()
+    //  def thirdDay = currDate.with(TemporalAdjusters.dayOfWeekInMonth(3, DayOfWeek.SUNDAY));
+    def l_date = nthWeekdayOfMonth(6, 2, 2022, TimeZone.getTimeZone("Europe/London"))
     if (currDate.equals(thirdDay)) {
         flag = true
     }
 
     echo "${flag}"
+
+    echo "${l_date}"
 
 
     Jenkins.instance.getAllItems(AbstractItem.class).each {
@@ -22,7 +20,15 @@ def call(names) {
     };
 }
 
-
+def nthWeekdayOfMonth(int dayOfWeek, int month, int year, int week, TimeZone timeZone) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTimeZone(timeZone);
+    calendar.set(Calendar.DAY_OF_WEEK, dayOfWeek);
+    calendar.set(Calendar.WEEK_OF_MONTH, week);
+    calendar.set(Calendar.MONTH, month);
+    calendar.set(Calendar.YEAR, year);
+    return calendar.getTime();
+}
 
 
 
