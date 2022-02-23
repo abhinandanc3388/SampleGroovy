2f0531ffae65cc265bfce1d71ad8afda258ae5f6
abhinandan <abhinandanc3388@gmail.com>
2022-02-23 19:26:53 +0530

    commit11

---
+++ vars/testlib.groovy
@@ -0,0 +1,26 @@
+def call(names) {
+    echo "Hey ${names} , This is test  lib?"
+
+    def flag = false
+
+    //  def currDate = LocalDate.now()
+    //  def thirdDay = currDate.with(TemporalAdjusters.dayOfWeekInMonth(3, DayOfWeek.SUNDAY));
+/*    def l_date = nthWeekdayOfMonth(6, 2, 2022, TimeZone.getTimeZone("Europe/London"))
+    if (currDate.equals(thirdDay)) {
+        flag = true
+    }
+
+    echo "${flag}"
+
+    echo "${l_date}"
+
+
+    Jenkins.instance.getAllItems(AbstractItem.class).each {
+        println it.fullName + " - " + it.class
+    };*/
+}
+
+
+
+
+
